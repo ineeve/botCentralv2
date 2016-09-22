3ea2dcde8bd9dc82ffb09fefb9a03248b7c13a65
Renato Campos <renatoineeve@gmail.com>
2016-09-22 09:59:09 +0100

    Pro

---
+++ createTable.sql
@@ -0,0 +1,13 @@
+CREATE TABLE x_world (
+    a    INT,
+    b    INT,
+    c    INT,
+    d    INT,
+    e    INT,
+    f    TEXT,
+    g    INT,
+    h    TEXT,
+    i    INT,
+    clan TEXT,
+    pop  INT
+);
